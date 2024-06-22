b2706de7fcd46c08fffa8dbe17ad51779183b600
BenMonroy <135274492+BenMonroy@users.noreply.github.com>
2024-06-22 10:59:56 -0400

    Add files via upload

---
+++ frontend/lib/login.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'mongo.dart';
+import 'home.dart';
+import 'signup.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text('Login', style: TextStyle(fontSize: 24)),
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(hintText: 'Username'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your username';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(hintText: 'Password'),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState?.validate() == true) {
+                      //TODO add the api here to log in 
+                      // var db = MongoDatabase();
+                      // var user = db.loginUser(_usernameController, _passwordController);
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => HomeScreen()));
+                    }
+                  },
+                  child: const Text('Login'),
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => SignUp()));
+                    },
+                    child: Text('Don\'t have an account? Sign up here')),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
