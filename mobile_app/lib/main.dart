cfcc2c3dca996a49c148e2962d8ae9b771518704
Ben Monroy <135274492+BenMonroy@users.noreply.github.com>
2024-07-03 11:43:30 -0400

    put landing page before login page in page navigation

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/login.dart';
+import 'screens/landing_page.dart';
 
 
 void main() {
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Login()
+      home: LandingPage()
     );
   }
 }
