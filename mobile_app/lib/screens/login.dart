02dfcc93466adbc47fb2e9ba59b069f683a1fc27
Ben Monroy <135274492+BenMonroy@users.noreply.github.com>
2024-07-09 15:22:04 -0400

    Update login.dart
    added some styling to page

--- mobile_app/lib/screens/login.dart
+++ mobile_app/lib/screens/login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'navigation.dart';
+import 'package:gun/navigation.dart';
 //import 'mongo.dart';
 import 'signup.dart';
 
@@ -27,10 +27,19 @@ class _LoginState extends State<Login> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const Text('Login', style: TextStyle(fontSize: 24)),
+                const Text('Login',
+                    style: TextStyle(fontSize: 24, color: Colors.black)),
+                SizedBox(height: 40),
                 TextFormField(
                   controller: _usernameController,
-                  decoration: const InputDecoration(hintText: 'Username'),
+                  decoration: const InputDecoration(
+                      hintText: 'Username',
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: 1),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red, width: 1))),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'Please enter your username';
@@ -38,9 +47,17 @@ class _LoginState extends State<Login> {
                     return null;
                   },
                 ),
+                SizedBox(height: 10),
                 TextFormField(
                   controller: _passwordController,
-                  decoration: const InputDecoration(hintText: 'Password'),
+                  decoration: const InputDecoration(
+                      hintText: 'Password',
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: 1),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red, width: 1))),
                   obscureText: true,
                   validator: (value) {
                     if (value == null || value.isEmpty) {
@@ -49,28 +66,37 @@ class _LoginState extends State<Login> {
                     return null;
                   },
                 ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState?.validate() == true) {
-                      //TODO add the api here to log in w/ verification
-                       //var db = MongoDatabase();
-                      // var user = await db.loginUser(_usernameController.text, _passwordController.text);
-                      // if(user != 'User not found'){
-                      Navigator.of(context).pushReplacement(MaterialPageRoute(
-                          builder: (context) => NavigationPage()));
-                       //} //TODO errro message for incorrect login
-                    }
-                  },
-                  child: const Text('Login'),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: 400,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white),
+                    onPressed: () async {
+                      if (_formKey.currentState?.validate() == true) {
+                        //TODO add the api here to log in w/ verification
+                        //var db = MongoDatabase();
+                        // var user = await db.loginUser(_usernameController.text, _passwordController.text);
+                        // if(user != 'User not found'){
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (context) => NavigationPage()));
+                        //} //TODO errro message for incorrect login
+                      }
+                    },
+                    child: const Text('Login'),
+                  ),
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 5),
                 TextButton(
+                    style: TextButton.styleFrom(foregroundColor: Colors.black),
                     onPressed: () {
                       Navigator.of(context).push(
                           MaterialPageRoute(builder: (context) => SignUp()));
                     },
-                    child: Text('Don\'t have an account? Sign up here')),
+                    child: Text(
+                      'Don\'t have an account? Register here',
+                    )),
               ],
             ),
           ),
