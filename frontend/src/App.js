b605855285d63fa3d0a46d41340999025bdb3a81
aletej <139593934+aletejeira@users.noreply.github.com>
2024-07-11 18:33:27 -0400

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,25 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
+import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
-import CardPage from './pages/CardPage';
+import SignupPage from './pages/SignupPage';
+import HomePage from './pages/HomePage';
+import ArmoryPage from './pages/ArmoryPage';
 
-function App() {
+function App()
+{
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/cards" element={<CardPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Signup" element={<SignupPage />} />
+        <Route path="/Home" element={<HomePage  />} />
+        <Route path="/Armory" element={<ArmoryPage />} />
       </Routes>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
