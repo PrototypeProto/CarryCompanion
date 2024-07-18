aeb6247932d978eff847f6368a70d7f33c6b0e67
BenMonroy <ben.a.monroy@gmail.com>
2024-07-18 13:24:58 -0400

    styled login, signup more

--- mobile_app/lib/screens/on_app_launch/login.dart
+++ mobile_app/lib/screens/on_app_launch/login.dart
@@ -17,90 +17,104 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-        child: Padding(
+        child: SingleChildScrollView(
           padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text('Login',
-                    style: TextStyle(fontSize: 24, color: Colors.black)),
-                SizedBox(height: 40),
-                TextFormField(
-                  controller: _usernameController,
-                  decoration: const InputDecoration(
-                      hintText: 'Username',
-                      filled: true,
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black, width: 1),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.red, width: 1))),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your username';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 10),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: const InputDecoration(
-                      hintText: 'Password',
-                      filled: true,
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black, width: 1),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.red, width: 1))),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  width: 400,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red,
-                        foregroundColor: Colors.white),
-                    onPressed: () async {
-                      if (_formKey.currentState?.validate() == true) {
-                        //TODO add the api here to log in w/ verification
-                        var db = MongoDatabase();
-                        var user = await db.loginUser(_usernameController.text, _passwordController.text);
-                        if(user != 'User not found'){
-                          Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) => NavBar()));
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Failed to Login')),); 
+          child: Column(
+            children: [
+              SizedBox(height: screenHeight * 0.125),
+              Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text('Login',
+                        style: TextStyle(fontSize: 24, color: Colors.black)),
+                    const SizedBox(height: 40),
+                    TextFormField(
+                      controller: _usernameController,
+                      decoration: const InputDecoration(
+                          hintText: 'Username',
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black, width: 1),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red, width: 1))),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your username';
                         }
-                      }
-                    },
-                    child: const Text('Login'),
-                  ),
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration: const InputDecoration(
+                          hintText: 'Password',
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black, width: 1),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red, width: 1))),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: 400,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red,
+                            foregroundColor: Colors.white),
+                        onPressed: () async {
+                          if (_formKey.currentState?.validate() == true) {
+                            var db = MongoDatabase();
+                            var user = await db.loginUser(
+                                _usernameController.text,
+                                _passwordController.text);
+                            if (user != 'User not found') {
+                              Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (context) => NavBar()));
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('Failed to Login')),
+                              );
+                            }
+                          }
+                        },
+                        child: const Text('Login'),
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    TextButton(
+                        style: TextButton.styleFrom(
+                            foregroundColor: Colors.black),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => SignUp()));
+                        },
+                        child: const Text(
+                          'Don\'t have an account? Register here',
+                        )),
+                  ],
                 ),
-                const SizedBox(height: 5),
-                TextButton(
-                    style: TextButton.styleFrom(foregroundColor: Colors.black),
-                    onPressed: () {
-                      Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => SignUp()));
-                    },
-                    child: Text(
-                      'Don\'t have an account? Register here',
-                    )),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
